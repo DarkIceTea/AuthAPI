55071b52edd550a147e46ee322a767f3cab9b67b
DarkIce <hipbarchuk@gmail.com>
2024-12-04 17:33:31 +0300

    add test for RefreshTokenServivce using bogus

---
+++ InfrastructureTests/Services/RefreshTokenServiceTests.cs
@@ -0,0 +1,32 @@
+﻿using Application.Abstractions;
+using Application.Services;
+using Domain.Models;
+using Moq;
+
+namespace InfrastructureTests.Services
+{
+    public class RefreshTokenServiceTests
+    {
+        Mock<IRefreshTokenRepository> repoMock;
+        public RefreshTokenServiceTests()
+        {
+            repoMock = new Mock<IRefreshTokenRepository>();
+        }
+
+        [Theory]
+        [ClassData(typeof(RefreshTokenServiceTestsData))]
+        public async Task CreateRefreshTokenAsync_ShouldReturnRefreshToken(CustomUser user)
+        {
+            //Arrange
+            var refTokServ = new RefreshTokenService(repoMock.Object);
+
+            //Act
+            var token = await refTokServ.CreateRefreshTokenAsync(user, CancellationToken.None);
+
+            //Assert
+            //Assert.NotNull(token);
+            Assert.IsType<RefreshToken>(token);
+            repoMock.Verify(r => r.CreateTokenAsync(token, CancellationToken.None), Times.Once);
+        }
+    }
+}
---
+++ InfrastructureTests/Services/RefreshTokenServiceTestsData.cs
@@ -0,0 +1,29 @@
+﻿using Bogus;
+using Domain.Models;
+using System.Collections;
+
+namespace InfrastructureTests.Services
+{
+    public class RefreshTokenServiceTestsData : IEnumerable<object[]>
+    {
+        Faker<CustomUser> customUserFaker;
+        readonly List<object[]> _data = new List<object[]>();
+        public RefreshTokenServiceTestsData()
+        {
+            customUserFaker = new Faker<CustomUser>()
+                .RuleFor(u => u.Id, Guid.NewGuid)
+                .RuleFor(u => u.Email, f => f.Internet.Email())
+                .RuleFor(u => u.UserName, f => f.Internet.UserName());
+
+            var randUsr = customUserFaker.Generate(10);
+            foreach (var item in randUsr)
+            {
+                var obj = new object[] { item };
+                _data.Add(obj);
+            }
+        }
+        public IEnumerator<object[]> GetEnumerator() => _data.GetEnumerator();
+
+        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+    }
+}
